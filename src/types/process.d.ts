7442057688d553115ce73bfbeca81c5b8bdda362
Illia <60040628+Barto-dev@users.noreply.github.com>
2024-09-28 08:41:39 +0100

    feat: added postgres

--- src/types/process.d.ts
+++ src/types/process.d.ts
@@ -4,5 +4,6 @@ declare namespace NodeJS {
     NEXT_PUBLIC_UNSPLASH_ACCESS_KEY: string;
     NEXT_PUBLIC_UNSPLASH_SECRET_KEY: string;
     REPLICATE_API_TOKEN: string;
+    DATABASE_URL: string;
   }
 }
